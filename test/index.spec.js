49eddcc02b28ba46cf92d46f4f16544da70d44b0
Matthew Lawrence-Stubley <quakematt@outlook.com>
2015-04-23 20:26:38 +0100

    Prepare working environment

---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+// mtl/promise
+describe('mtl-promise', function() {
+
+	var promise = require('../src/index');
+
+});
