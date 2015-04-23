51329987e020aec547c3c935e08aa715a6dd998b
Matthew Lawrence-Stubley <quakematt@outlook.com>
2015-04-23 22:24:53 +0100

    Integrate Promiz library (https://github.com/Zolmeister/promiz)

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,50 @@
 'use strict';
 
-var expect = require('chai').expect;
-var sinon = require('sinon');
-
 // mtl/promise
 describe('mtl-promise', function() {
 
-	var promise = require('../src/index');
-
+	var Promise = require('../src/index');
+	
+	var adapter = {
+		
+		resolved: function(value) {
+			
+			return new Promise(function(resolve) {
+				resolve(value);
+			});
+			
+		},
+		
+		rejected: function(reason) {
+			
+			return new Promise(function(resolve, reject) {
+				reject(reason);
+			});
+			
+		},
+		
+		deferred: function() {
+			
+			var deferred = {};
+			
+			deferred.promise = new Promise(function(resolve, reject) {
+				
+				deferred.resolve = function(value) {
+					resolve(value);
+				};
+				
+				deferred.reject = function(reason) {
+					reject(reason);
+				};
+				
+			});
+		
+			return deferred;
+			
+		}
+		
+	};
+	
+	require('promises-aplus-tests').mocha(adapter);
+	
 });
